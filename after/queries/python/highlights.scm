36225790382c08a9891b0f92a68435c7927e0270
Brandt Buchda <brandt.buchda@gmail.com>
2026-04-19 15:56:56 -0500

    more rules

--- after/queries/python/highlights.scm
+++ after/queries/python/highlights.scm
@@ -1,7 +1,24 @@
-; Extend default Python highlights:
-;   * Allow leading underscore(s) on SCREAMING_CASE constants.
-;     Default captures ^[A-Z][A-Z0-9_]*$ which excludes _SOME_CONSTANT.
+; Extend default Python highlights.
+;
+; NOTE: predicates
+;   #match?      -> vim regex (needs \+ for one-or-more)
+;   #lua-match?  -> lua patterns (+ works). Registered by nvim-treesitter.
+; Priority bumped above the default @variable catch-all (which is 100).
 
+; --- Constants: _CONST / __CONST / CONST ----------------------------------------
 ((identifier) @constant
- (#match? @constant "^_+[A-Z][A-Z0-9_]*$")
- (#set! "priority" 120))
+ (#lua-match? @constant "^_*[A-Z][A-Z0-9_]*$")
+ (#set! "priority" 125))
+
+; --- Dict keys: string literals and identifier shorthand -------------------------
+(dictionary
+  (pair
+    key: (string) @property))
+
+(dictionary
+  (pair
+    key: (identifier) @property))
+
+; keyword arguments (foo(key=value)) → parameter
+(keyword_argument
+  name: (identifier) @variable.parameter)
--- after/queries/python/injections.scm
+++ after/queries/python/injections.scm
@@ -1,102 +1,91 @@
-; Extend default Python injections with:
+; Extend default Python injections:
 ;   1. # language=<lang>  directive comments
-;   2. Variable-name heuristics: *_sql / *_query -> sql,
-;                                *_json          -> json,
-;                                *_html          -> html,
-;                                *_xml           -> xml,
-;                                *_yaml / *_yml  -> yaml,
-;                                *_css           -> css,
-;                                *_js            -> javascript
+;   2. Variable-name heuristics
+;
+; Uses #lua-match? (Lua patterns via nvim-treesitter). Uses the whole
+; (string) node with injection.include-children so triple-quoted strings
+; with newlines / escapes are captured as a single block.
 
 ; --- # language=<lang> directive ---------------------------------------------
-; Usage:
-;   # language=sql
-;   query = """SELECT * FROM t"""
 
 ((comment) @injection.language
  .
  (expression_statement
    (assignment
-     right: [
-       (string (string_content) @injection.content)
-       (concatenated_string (string (string_content) @injection.content))
-     ]))
- (#match? @injection.language "language=[%w_]+")
+     right: (string) @injection.content))
+ (#lua-match? @injection.language "language=[%w_]+")
  (#gsub! @injection.language ".*language=([%w_]+).*" "%1")
- (#set! injection.combined))
+ (#set! injection.include-children))
 
 ((comment) @injection.language
  .
  (expression_statement
-   (string (string_content) @injection.content))
- (#match? @injection.language "language=[%w_]+")
+   (string) @injection.content)
+ (#lua-match? @injection.language "language=[%w_]+")
  (#gsub! @injection.language ".*language=([%w_]+).*" "%1")
- (#set! injection.combined))
+ (#set! injection.include-children))
 
 ; --- Name-based heuristics ----------------------------------------------------
-; Using injection.combined so that triple-quoted strings, which may contain
-; multiple string_content chunks split by escape sequences / interpolation,
-; are treated as a single injected block.
 
 ; *_sql / *_query -> sql
 ((assignment
    left: (identifier) @_name
-   right: (string (string_content) @injection.content))
- (#match? @_name "[sS][qQ][lL]$")
+   right: (string) @injection.content)
+ (#lua-match? @_name "[sS][qQ][lL]$")
  (#set! injection.language "sql")
- (#set! injection.combined))
+ (#set! injection.include-children))
 
 ((assignment
    left: (identifier) @_name
-   right: (string (string_content) @injection.content))
- (#match? @_name "[qQ][uU][eE][rR][yY]$")
+   right: (string) @injection.content)
+ (#lua-match? @_name "[qQ][uU][eE][rR][yY]$")
  (#set! injection.language "sql")
- (#set! injection.combined))
+ (#set! injection.include-children))
 
 ; *_json -> json
 ((assignment
    left: (identifier) @_name
-   right: (string (string_content) @injection.content))
- (#match? @_name "[jJ][sS][oO][nN]$")
+   right: (string) @injection.content)
+ (#lua-match? @_name "[jJ][sS][oO][nN]$")
  (#set! injection.language "json")
- (#set! injection.combined))
+ (#set! injection.include-children))
 
 ; *_html -> html
 ((assignment
    left: (identifier) @_name
-   right: (string (string_content) @injection.content))
- (#match? @_name "[hH][tT][mM][lL]$")
+   right: (string) @injection.content)
+ (#lua-match? @_name "[hH][tT][mM][lL]$")
  (#set! injection.language "html")
- (#set! injection.combined))
+ (#set! injection.include-children))
 
 ; *_xml -> xml
 ((assignment
    left: (identifier) @_name
-   right: (string (string_content) @injection.content))
- (#match? @_name "[xX][mM][lL]$")
+   right: (string) @injection.content)
+ (#lua-match? @_name "[xX][mM][lL]$")
  (#set! injection.language "xml")
- (#set! injection.combined))
+ (#set! injection.include-children))
 
 ; *_yaml / *_yml -> yaml
 ((assignment
    left: (identifier) @_name
-   right: (string (string_content) @injection.content))
- (#match? @_name "[yY][aA]?[mM][lL]$")
+   right: (string) @injection.content)
+ (#lua-match? @_name "[yY][aA]?[mM][lL]$")
  (#set! injection.language "yaml")
- (#set! injection.combined))
+ (#set! injection.include-children))
 
 ; *_css -> css
 ((assignment
    left: (identifier) @_name
-   right: (string (string_content) @injection.content))
- (#match? @_name "[cC][sS][sS]$")
+   right: (string) @injection.content)
+ (#lua-match? @_name "[cC][sS][sS]$")
  (#set! injection.language "css")
- (#set! injection.combined))
+ (#set! injection.include-children))
 
 ; *_js -> javascript
 ((assignment
    left: (identifier) @_name
-   right: (string (string_content) @injection.content))
- (#match? @_name "[jJ][sS]$")
+   right: (string) @injection.content)
+ (#lua-match? @_name "[jJ][sS]$")
  (#set! injection.language "javascript")
- (#set! injection.combined))
+ (#set! injection.include-children))
